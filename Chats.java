902fe797fad616ffacae044a8ab105b4eb6c8288
Raul Garcia <39319153+raulyyg@users.noreply.github.com>
2020-01-26 01:22:59 -0800

    Add files via upload

---
+++ Chats.java
@@ -0,0 +1,19 @@
+/*
+ * RAUL GARCIA
+ * CS 351 
+ * NOV 27, 2017
+ * COPYRIGHT RAUL GARCIA 2017
+ */
+
+package chats;
+
+public class Chats {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
